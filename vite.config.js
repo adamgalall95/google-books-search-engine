43cfaff38d05b012b05b1ac5d626cf373039ce9b
Adam Galall <145528713+adamgalall95@users.noreply.github.com>
2026-09-25 11:22:43 +0930

    Set base path for deployment in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/google-books-search-engine/",
   plugins: [react()],
   test: {
-    globals: true, //- get rid of imports in every file
+    globals: true,
     environment: "jsdom",
     setupFiles: "./config/setup.js",
   },
